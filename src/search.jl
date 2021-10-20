53168629ba71ee904359bc7c5fdf4b3d6802eecc
Eric S. Tellez <donsadit@gmail.com>
2021-10-20 08:20:00 -0500

    increases version

--- src/search.jl
+++ src/search.jl
@@ -43,42 +43,6 @@ end
 
 Searches `q` in `idx` using the cosine dissimilarity, it computes the full operation on `idx`. `res` specify the query
 """
-#=function usearch(idx::InvertedFile, q::DVEC, res::KnnResult)
-	L = prepare_posting_lists_for_querying(idx, q)
-	sort!(L, by=first)
-    P = ones(Int, length(L))
-
-    @inbounds while true
-        _remove_empty!(P, L)
-        n = length(P)
-        n == 0 && break
-        n > 1 && _sort!(P, L)
-        plist = L[1]
-		_p = P[1]
-        id = plist.I[_p]
-		w = plist.W[_p]
-        weight = convert(Float64, plist.weight * w)
-        P[1] += 1
-
-        for i in 2:n
-            plist = L[i]
-			_p = P[i]
-			id_ = plist.I[_p]
-			w = plist.W[_p]
-            if id == id_
-                weight += plist.weight * w
-                P[i] = _p + 1
-			else
-				break
-			end
-        end
-
-		push!(res, id, 1.0 - weight)
-    end
-
-    res
-
-end =#
 
 function usearch(idx::InvertedFile, q::DVEC, res::KnnResult)
 	Q = prepare_posting_lists_for_querying(idx, q)
